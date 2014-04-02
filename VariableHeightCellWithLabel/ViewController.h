813ac7f95e60c311fcadd76454fc31035664be26
Claes Lillieskold <claes.lillieskold@bontouch.com>
2014-04-02 13:24:20 +0200

    Initial Commit

---
+++ VariableHeightCellWithLabel/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  VariableHeightCellWithLabel
+//
+//  Created by Claes Lillieskold on 2014-04-02.
+//  Copyright (c) 2014 Claes Lillieskold. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ VariableHeightCellWithLabel/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  VariableHeightCellWithLabel
+//
+//  Created by Claes Lillieskold on 2014-04-02.
+//  Copyright (c) 2014 Claes Lillieskold. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
